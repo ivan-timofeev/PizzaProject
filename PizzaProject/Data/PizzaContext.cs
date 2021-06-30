be7bb761986f69a5546a6d3a1624e1b510b06c4f
aws <qwark4media@gmail.com>
2021-06-30 14:49:17 +0300

    fixed db initializer

--- PizzaProject/Data/PizzaContext.cs
+++ PizzaProject/Data/PizzaContext.cs
@@ -19,7 +19,7 @@ namespace PizzaProject.Data
                     new Pizza { Id = 1, Name = "Пицца Барбекю", PictureUrl = "/img/1.jpeg", Price = 750, Description = "Большая пицца Барбекю, 35см, 630г." },
                     new Pizza { Id = 2, Name = "Пицца Сырная", PictureUrl = "/img/2.jpeg", Price = 410, Description = "Большая пицца Сырная, 35см, 540г."},
                     new Pizza { Id = 3, Name = "Пицца Пеперони", PictureUrl = "/img/3.jpeg", Price = 500, Description = "Большая пицца Пеперони, 35см, 570г."},
-                    new Pizza { Id = 3, Name = "Пицца Ветчина и сыр", PictureUrl = "/img/4.jpeg", Price = 500, Description = "Большая пицца Ветчина и сыр, 35см, 570г."}
+                    new Pizza { Id = 4, Name = "Пицца Ветчина и сыр", PictureUrl = "/img/4.jpeg", Price = 500, Description = "Большая пицца Ветчина и сыр, 35см, 570г."}
                 });
         }
 
--- PizzaProject/Migrations/20210630114758_test.cs
+++ PizzaProject/Migrations/20210630114758_test.cs
@@ -2,7 +2,7 @@
 
 namespace PizzaProject.Migrations
 {
-    public partial class InitialCreate : Migration
+    public partial class test : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -13,13 +13,24 @@ namespace PizzaProject.Migrations
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Discription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     PictureUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Price = table.Column<decimal>(type: "decimal(18,4)", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_PizzaList", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "PizzaList",
+                columns: new[] { "Id", "Description", "Name", "PictureUrl", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Большая пицца Барбекю, 35см, 630г.", "Пицца Барбекю", "/img/1.jpeg", 750m },
+                    { 2, "Большая пицца Сырная, 35см, 540г.", "Пицца Сырная", "/img/2.jpeg", 410m },
+                    { 3, "Большая пицца Пеперони, 35см, 570г.", "Пицца Пеперони", "/img/3.jpeg", 500m },
+                    { 4, "Большая пицца Ветчина и сыр, 35см, 570г.", "Пицца Ветчина и сыр", "/img/4.jpeg", 500m }
                 });
         }
 
