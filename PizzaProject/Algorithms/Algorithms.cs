43c154a785b13d319cb65b84d47a6f1eed6074dd
aws <qwark4media@gmail.com>
2021-07-11 22:50:46 +0300

    в алгоритмы добавлен метод хеширования паролей

--- PizzaProject/Algorithms/Algorithms.cs
+++ PizzaProject/Algorithms/Algorithms.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace PizzaProject.Utils
@@ -49,6 +51,25 @@ namespace PizzaProject.Utils
             }
 
             return matrixD[n - 1, m - 1];
+        }
+
+        public static string CalcPasswordHash(string password)
+        {
+            byte[] salt = new byte[128 / 8];
+
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+            return hashed;
         }
     }
 }
